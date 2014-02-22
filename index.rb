9e83daa61c9f196317bc82498e75c3d5120b6f9c
Julio <ljuliom@gmail.com>
2014-02-22 14:30:16 -0500

    finalpush

--- index.rb
+++ index.rb
@@ -1,5 +1,5 @@
 require 'koala'
-@graph = Koala::Facebook::API.new('CAACEdEose0cBAIkhNcELtQymYRt9JwFfdEd68sdTCueZAT4RDfaIB11dX3vSqZARdKgOBeLV5F5xx0TZBo4GkZCFNmDFCSVFgfWWXPSRleQdSxqC3jrMa3Vpw11gZCy6wMKEdd9hwWKQ3aRiHZBhyClTVhACOeD0Jt9tqq66AmtTUUdhCgrY2VZCbB8l9az0MrlkBoA5hvvQwZDZD')
+@graph = Koala::Facebook::API.new(your_access_tokken)
 profile = @graph.get_connections('me','friends')
 #profile = @graph.get_connections('me', 'feed').first["message"]
 puts profile
